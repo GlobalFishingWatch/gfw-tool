8628c6b8c0a3045c39cad49de9445665833189bc
aperdizs <info@alvaroperdiz.com>
2022-11-17 19:24:48 +0100

    Add bigquery commands as a package

---
+++ pkg/bigquery.go
@@ -0,0 +1,18 @@
+package pkg
+
+import (
+	"github.com/GlobalFishingWatch/gfw-tool/internal/action/bigquery"
+	"github.com/GlobalFishingWatch/gfw-tool/types"
+)
+
+func BigQueryCreateTable(params types.BQCreateTableConfig) {
+	bigquery.ExecuteCreateTable(params)
+}
+
+func BigQueryCreateTemporalTable(params types.BQCreateTemporalTableConfig) {
+	bigquery.ExecuteCreateTemporalTable(params)
+}
+
+func BigQueryExecuteRawQuery(params types.BQRawQueryConfig) {
+	bigquery.ExecuteRawQuery(params)
+}
