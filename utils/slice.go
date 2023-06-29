e59b37e7a048d936d3522465e7d1928eb540e743
Raul <rrequero@gmail.com>
2023-06-29 13:22:52 +0200

    remove unnnecesary check of nil

--- utils/slice.go
+++ utils/slice.go
@@ -5,7 +5,7 @@ import "strings"
 func ConvertSliceToMap(labels []string) map[string]string {
 	result := map[string]string{}
 
-	if labels != nil && len(labels) > 0 {
+	if len(labels) > 0 {
 		for _, label := range labels {
 			parts := strings.Split(label, "=")
 			if len(parts) != 2 {
